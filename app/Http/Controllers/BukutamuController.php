56214c5c41d33d76a19f7074597dbb05176dac60
KevinIlpz <kevin.02.april@gmail.com>
2024-08-29 11:38:12 +0700

    migration

---
+++ app/Http/Controllers/BukutamuController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Bukutamu;
+use Illuminate\Http\Request;
+
+class BukutamuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Bukutamu $bukutamu)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Bukutamu $bukutamu)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Bukutamu $bukutamu)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Bukutamu $bukutamu)
+    {
+        //
+    }
+}
